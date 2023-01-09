b4dd8d5a639baf34b93c348c807bd346638fd7db
mikzanco <mikyzancolo97@gmail.com>
2023-01-09 19:21:18 +0100

    fix problem drive and print in page film's title

--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -3,3 +3,11 @@
 @section('content')
     <h1>Elenco film</h1>
 
+    <ul>
+        @foreach ($movies as  $movie)
+
+            <li>{{ $movie->title }}</li>
+        @endforeach
+    </ul>
+
+@endsection
